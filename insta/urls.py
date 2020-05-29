673c130226fa934d2864d5b4259e4bbb38289157
kangangi <wanjikukangangi@gmail.com>
2020-05-29 15:55:11 +0300

    add basic views

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from . import views 
 
-urlpattern = []
+urlpatterns = [
+    path('',views.home , name = "home"),
+]
 
 if settings.DEBUG:
-    urlpattern+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    '''
+    Displays home page
+    '''
+    title = "Finsta"
+    
+    return render(request, "home.html")
