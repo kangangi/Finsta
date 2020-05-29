9b83c0a79fd31479c4c0a45759955a6c49c1f454
kangangi <wanjikukangangi@gmail.com>
2020-05-29 15:18:45 +0300

    add admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Comment, Profile, Image
 
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Image)
