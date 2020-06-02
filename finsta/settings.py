7c06cfe09f0e25a322354ec4e34851f442eab014
kangangi <wanjikukangangi@gmail.com>
2020-06-02 10:24:54 +0300

    deploy to heroku

--- finsta/settings.py
+++ finsta/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USERNAME': os.environ.get('DB_USER'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
